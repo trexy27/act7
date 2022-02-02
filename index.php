3356160427cfca52dbaf9ea0b3b2b30f0775c534
trexy27 <79253445+trexy27@users.noreply.github.com>
2022-02-02 13:56:03 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,79 @@
+<?php 
+
+
+$conn = new mysqli('sql103.epizy.com', 'epiz_30965136', 'iv4f8e1giUdRq9', 'epiz_30965136_act7');
+
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Activity 7</title>
+        <link rel="icon" href="logo.png" type="image/x-icon">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Monda&display=swap" rel="stylesheet">
+
+        <style type="text/css">
+            body,html{
+            min-height: 100%;
+            }
+            body
+            {
+                margin: 0;
+                padding: 0;
+                background-color: #c9926b;
+                background-repeat: no-repeat;
+                background-size: cover;
+                background-position: center;
+                font-family: sans-serif;
+            }
+            .box
+            {
+                width: 900px;
+                height: 700px;
+                background:#9e4237;
+                color: white ;
+                top: 50%;
+                left: 50%;
+                position: absolute;
+                transform: translate(-50%, -50%);
+                box-sizing: border-box;
+                padding: 90px 30px;
+            }
+            .avatar
+            {
+                width: 100px;
+                height: 100px;
+                border-radius: 50%;
+                position: absolute;
+                top: -50px;
+            }
+            h1
+            {
+                margin: 0;
+                padding: 0 0 20px;
+                text-align: left;
+                font-size: 48px;
+                font-family: 'Monda', sans-serif;
+            }
+
+        </style>
+
+
+  
+        <body>
+        	<div class="box">        			
+        		<img src="logo.png" class="avatar">
+        		<h1>
+        			<br>Name: Trexy Mae O. Napigkit</br>
+                    <br>Course:BSIT 3R5 </br>
+                    <br>Address: Zone 7, Bulua Villa Candida Subd. Cdoc</br></h1>
+	
+            </div>
+        </body>
+    </head>
+</html>
+
